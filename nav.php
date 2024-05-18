d4457c7f2f2ef2b0da291ec6722e87eb37135c29
Erik Flores <erikflores.code@gmail.com>
2024-05-18 16:33:12 -0400

    Primer commit

---
+++ nav.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container_12">
+      <div class="grid_12">
+        <h1><a href="index.php"><img src="images/logo.png" alt=""></a></h1>
+        <div class="menu_block">
+          <nav>
+            <ul class="sf-menu">
+              <li ><a href="index.php">Inicio</a></li>
+              <li class="with_ul"><a href="about-us.php">Servicios</a>
+                <ul>
+                  <li><a href="#">Reservar</a></li>
+                </ul>
+              </li>
+              <li><a href="menu.php">Menu</a></li>
+              <li><a href="contacts.php">Contactos</a></li>
+            </ul>
+          </nav>
+          <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+  </header>
